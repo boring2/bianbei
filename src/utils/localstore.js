95c2d115c32b1fd3acd6a537c928ce4e7ea5065e
houser <houser@xmind.net>
2018-07-18 18:05:31 +0800

    add local api

--- src/utils/localstore.js
+++ src/utils/localstore.js
@@ -1,3 +1,5 @@
+import { version } from "punycode";
+
 /**
  * 一个完整的topic
  * Topic {
@@ -5,26 +7,55 @@
  * }
  */
 let LocalStore = {
-  fetchTopic (id) {
-    // wx.get
+  fetchTopic (topicId, success, error) {
+    return this.getTopic(topicId, success, error)
   },
-  saveTopic(id, data, success, error) {
+
+  getTopic (topicId, success, error) {
     try {
-      wx.setStorageSync(id, data)
+      let topic = wx.getStorageSync(topicId)
+      success && success()
+      return topic
+    } catch (e) {
+      console.error(getTopic, e)
+      error && error()
+    }
+  },
+
+  saveTopic(topicId, data, success, error) {
+    try {
+      wx.setStorageSync(topicId, data)
       success && success()
     } catch (e) {
       console.error(e)
       error && error()
     }
   },
-  addIdea(topicId, versionIndex, idea) {
-    try {
-      let topic = wx.getStorageSync(topicId)
-      topic.ideas[versionIndex].push(idea)
-      // wx.setStorageSync(topicId, )
-    } catch (error) {
 
+  /// 参数过多
+  addIdea(topicId, versionIndex, idea, isCheckout, sliceIndex, success, error) {
+    let topic = this.getTopic(topicId, success, error)
+    let ideas = topic.ideas[versionIndex]
+    if (!isCheckout) {
+      ideas.push(idea)
+    } else {
+      let sliceIdea = ideas.slice(0, sliceIndex)
+      let newVersionIndex = versionIndex + 1
+      let newIdeas = topic.ideas[newVersionIndex] || (topic.ideas[newVersionIndex] = sliceIdea)
+      newIdeas.push(sliceIdea)
     }
+    this.saveTopic(topicId, topic, success, error)
+  },
+
+  handleFeed (topicId, versionIndex, type, ideaIndex, success, error) {
+    let topic = this.getTopic(topicId, success, error)
+    let ideas = topic.ideas[versionIndex]
+    if (!ideas[ideaIndex][type]) {
+      ideas[ideaIndex][type] = 1
+    } else {
+      ideas[ideaIndex][type] += 1
+    }
+    this.saveTopic(topicId, topic, success, error)
   }
 }
 
