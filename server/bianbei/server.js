f9588e3367dbfb932324243c4b3d3ca43c9f0fa7
Boring <Boring@Boring.local>
2018-07-31 23:18:10 +0800

    add scroll bottom

--- server/bianbei/server.js
+++ server/bianbei/server.js
@@ -20,12 +20,12 @@ var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000)
 app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT)
 
-	// 注册全局未捕获异常处理器
+// 注册全局未捕获异常处理器
   process.on('uncaughtException', function(err) {
-  console.log('aaaaaaaaaaaaaaaaaaaaaa')
-  console.error('Caught exception:', err.stack)
-})
+    console.log('aaaaaaaaaaaaaaaaaaaaaa')
+    console.error('Caught exception:', err.stack)
+  })
   process.on('unhandledRejection', function(reason, p) {
-  console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack)
-})
+    console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack)
+  })
 })
